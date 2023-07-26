7907132caf28c5f656d6200ec04accffe4da56a3
sajalsa86e <sajalsm86@gmail.com>
2023-07-26 08:22:29 +0600

    update field number

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,40 @@
-
-document.getElementById('plusBtnMobile').addEventListener('click', function () {
-    const countInputField = document.getElementById('countMobile');
-    const countInputValueString = countInputField.value;
-    const privousCountInputValue = parseInt(countInputValueString);
-    const newCountInputValue = privousCountInputValue + 1;
+//utility
+function updateFieldNumber(fieldId, isIncrese) {
+    const getNumberField = document.getElementById(fieldId);
+    const getNumberString = getNumberField.value;
+    const previousFieldNumber = parseInt(getNumberString);
+    let newFieldNumber;
+    if (isIncrese === true) {
+        newFieldNumber = previousFieldNumber + 1;
+    }
+    else {
+        newFieldNumber = previousFieldNumber - 1;
+    }
     //set value
-    countInputField.value = newCountInputValue;
+    getNumberField.value = newFieldNumber;
+    return newFieldNumber;
+};
+
+//iPhone 11 128GB Black
+document.getElementById('plusBtnMobile').addEventListener('click', function () {
+
+    updateFieldNumber('countMobile', true);
 });
 
 document.getElementById('minusBtnMobile').addEventListener('click', function () {
-    const countInputField = document.getElementById('countMobile');
-    const countInputValueString = countInputField.value;
-    const privousCountInputValue = parseInt(countInputValueString);
-    const newCountInputValue = privousCountInputValue - 1;
-    //set value
-    countInputField.value = newCountInputValue;
+
+    updateFieldNumber('countMobile', false);
+
 });
+
+//iPhone 11 Silicone Case - Black
+document.getElementById('plusBtnCase').addEventListener('click', function () {
+
+    updateFieldNumber('countCase', true);
+
+});
+
+document.getElementById('minusBtnCase').addEventListener('click', function () {
+
+    updateFieldNumber('countCase', false);
+}); 
